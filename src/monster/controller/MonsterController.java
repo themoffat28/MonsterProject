22257cfe67434229cef787a8c05d00c4ad447006
themoffat28 <bthemoffat@gmail.com>
2016-09-16 08:53:22 -0600

    Gave user option of changing Gorgonzola's name.

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -1,14 +1,17 @@
 package monster.controller;
 
 import monster.model.MarshmallowMonster;
+import java.util.Scanner;
 
 public class MonsterController
 {
 	private MarshmallowMonster Gorgonzola;
+	private Scanner keyboardInput;
 	
 	public MonsterController()
 	{
 		Gorgonzola = new MarshmallowMonster("Gorgonzola", 6.0, 4, false, 0, 4); 
+		keyboardInput = new Scanner(System.in);
 	}	
 	                                                               
 	
@@ -20,5 +23,22 @@ public class MonsterController
 		System.out.println("doesn't have a belly button");
 		System.out.println(Gorgonzola.getArmCount() + " arms");
 		System.out.println("and " + Gorgonzola.getNoseCount() + " noses");
+		System.out.println("Do you want to change my name?");
+		String answer = keyboardInput.nextLine();
+			if(answer.equalsIgnoreCase("yes"))
+		{
+			System.out.println("Really? Alright, what should my name be?");
+			String newName = keyboardInput.nextLine();
+			Gorgonzola.setName(newName);
+		}
+			if(answer.equalsIgnoreCase("no"))
+			{
+				System.out.println("Thanks! I like my name.");
+			}
+			else
+			{
+				System.out.println("What?");
+			}
+				System.out.println(Gorgonzola);
 	}
 }
